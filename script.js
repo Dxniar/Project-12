08288a5b1439567c021a48dc31f9b1da1d757f70
Daniar <ayatdaniar@gmail.com>
2023-12-03 05:30:06 +0600

    if, else if, else,

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ console.log(numberOfFilms);
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+	actors: {},
+	genres: [],
+	privat: false
 };
 
 const a = prompt('Один из последних посмотренних фильмов?', ''),
-      b = prompt('Насколько оцените его?', ''),
-      c = prompt('Один из последних посмотренних фильмов?', ''),
-      d = prompt('Насколько оцените его?', '')
+	b = prompt('Насколько оцените его?', ''),
+	c = prompt('Один из последних посмотренних фильмов?', ''),
+	d = prompt('Насколько оцените его?', '')
 ;
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 
 console.log(personalMovieDB);
+
+
+// Условия if, else
+const number = 24;
+
+if (number == 25) {
+	console.log('Верно');
+} else if (number >= 0) {
+	console.log('Верно');
+} else {
+	console.log('ничего не верно');
+};
